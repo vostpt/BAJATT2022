7127171a65ea84ad653d0d14bc410761674c2d91
Jorge Gomes <jorge.gomes@vost.pt>
2022-03-17 13:48:05 +0000

    CONFIRM @ BAJA v 1.0
    Fully working version of CONFIRM for BAJA TT 2022 by ACP MOTORSPORTS

--- app.py
+++ app.py
@@ -30,44 +30,31 @@ server = app.server
 
 CONFIRM_LOGO = app.get_asset_url('CONFIRM_Logotype.png')
 
-# DATA TREATMENT
-
-
-df_ss1_cc = pd.read_csv('ss1_cc.csv')
-
-
-
-df_live = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSlasVmsQglAUGhesSL9_bJHaZ4rsbNG0m2U5nTFj25LyBdylAPiRvt2eDIVjfFQ7yI_ElWDus-qx2b/pub?gid=509896708&single=true&output=csv')
-
-
-total_incidents = str(round(df_live['INCIDENT SUM'].sum()))
-
-total_warnings = df_live.loc[df_live['INCIDENT UNIQUE'] == "WARNING", 'INCIDENT SUM'].sum()
-
-total_breakdowns = df_live.loc[df_live['INCIDENT UNIQUE'] == "Avaria", 'INCIDENT SUM'].sum()
-
-total_mech = df_live.loc[df_live['INCIDENT UNIQUE'] == "Avaria Mecânica", 'INCIDENT SUM'].sum()
-
-total_out = df_live.loc[df_live['INCIDENT UNIQUE'] == "Despiste", 'INCIDENT SUM'].sum()
-
-total_gaveup = df_live.loc[df_live['INCIDENT UNIQUE'] == "Desistência", 'INCIDENT SUM'].sum()
-
-
-fig = px.scatter_mapbox(df_ss1_cc, lat="lat", lon="lon", hover_name="Name", hover_data=["Description"],
-                        color_discrete_sequence=["red"], zoom=8)
-fig.update_layout(mapbox_style="open-street-map")
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
+color_map = {
+    "WARNING":"#C81D25",
+    "AVARIA":"#DE6E4B",
+    "ACIDENTE":"#AC9FBB",
+    "AVARIA MECÂNICA":"#4F5D75",
+    "DESPISTE":"#2D3142",
+    "DESISTÊNCIA":"#242424"
+}
 
 
 app.layout = dbc.Container(
     [
         dbc.Row(
             [
+                # AUTOMATIC UPDATER 
+                dcc.Interval(
+                    id='interval-component',
+                    interval=60*1000, # in milliseconds
+                    n_intervals=0
+                ),
                 dbc.Col(
                     [
                         dbc.Row(
                             [
+                                dbc.Row(html.Hr()),
                                 dbc.Col(width=2,xs=12, sm=12,md=1,lg=1,xl=1),
                                 dbc.Col(html.H3("BAJA TT 2022"),width=4,xs=12, sm=12,md=4,lg=4,xl=4),
                                 dbc.Col(width=4,xs=12, sm=12,md=1,lg=4,xl=4),
@@ -80,17 +67,17 @@ app.layout = dbc.Container(
                     [
                         dbc.Col(width=2,xs=12, sm=12,md=1,lg=2,xl=1),
                         dbc.Col(
-                            html.H5("by VOST PORTUGAL ")
+                            html.P("CONFIRM by VOST PORTUGAL ")
                         ),
                     ],
                 ),
             ], 
-        style={"height": "20%", "background-color": "#242424"},
+        style={"height": "20%", "background-color": "#1D1E2C"},
         ),    
         dbc.Row(
             [
                 dbc.Col(
-                    dcc.Graph(id='map',figure=fig), width=2,xs=12, sm=12,md=12,lg=12,xl=4,
+                    dcc.Graph(id='map'), width=2,xs=12, sm=12,md=12,lg=12,xl=4,
                 ),
                 dbc.Col(
                     dbc.Row(
@@ -100,8 +87,8 @@ app.layout = dbc.Container(
                                         dbc.CardHeader("TOTAL INCIDENTS", style={"background": "#FF495C","color":"white"}),
                                         dbc.CardBody(
                                                     [
-                                                        html.H6("TOTAL ACIDENTES", style={"color":"#FF495C"}, className="card-title"),
-                                                        html.H4(id="total_incidents",children=total_incidents),
+                                                        html.H6("TOTAL INCIDENTES", style={"color":"#FF495C"}, className="card-title"),
+                                                        html.H4(id="totals"),
                                                     ],
 
                                         ),
@@ -113,7 +100,7 @@ app.layout = dbc.Container(
                                         dbc.CardBody(
                                                     [
                                                         html.H6("RACE DIRECTOR", style={"color":"#C81D25"}, className="card-title"),
-                                                        html.H4(id="total_warnings",children=total_warnings),
+                                                        html.H4(id="total_warnings"),
                                                     ],
 
                                         ),
@@ -125,7 +112,7 @@ app.layout = dbc.Container(
                                         dbc.CardBody(
                                                     [
                                                         html.H6("AVARIAS", style={"color":"#DE6E4B"}, className="card-title"),
-                                                        html.H4(id="total_breakdowns",children=total_breakdowns),
+                                                        html.H4(id="total_breakdowns"),
                                                     ],
 
                                         ),
@@ -141,11 +128,11 @@ app.layout = dbc.Container(
                         [
                             dbc.Card(
                                     [
-                                        dbc.CardHeader("TOTAL MECHANICAL BREAKDOWNS", style={"background": "#FF495C","color":"white"}),
+                                        dbc.CardHeader("TOTAL MECHANICAL BREAKDOWNS", style={"background": "#4F5D75","color":"white"}),
                                         dbc.CardBody(
                                                     [
-                                                        html.H6("AVARIA MECÂNICA", style={"color":"#FF495C"}, className="card-title"),
-                                                        html.H4(id="total_mechs",children=total_mech),
+                                                        html.H6("AVARIA MECÂNICA", style={"color":"#4F5D75"}, className="card-title"),
+                                                        html.H4(id="total_mechs"),
                                                     ],
 
                                         ),
@@ -153,11 +140,11 @@ app.layout = dbc.Container(
                             ),
                             dbc.Card(
                                     [
-                                        dbc.CardHeader("OFF PISTE", style={"background": "#C81D25","color":"white"}),
+                                        dbc.CardHeader("OFF PISTE", style={"background": "#2D3142","color":"white"}),
                                         dbc.CardBody(
                                                     [
-                                                        html.H6("Despiste", style={"color":"#C81D25"}, className="card-title"),
-                                                        html.H4(id="total_outs",children=total_out),
+                                                        html.H6("DESPISTE", style={"color":"#2D3142"}, className="card-title"),
+                                                        html.H4(id="total_outs"),
                                                     ],
 
                                         ),
@@ -165,11 +152,11 @@ app.layout = dbc.Container(
                             ),
                             dbc.Card(
                                     [
-                                        dbc.CardHeader("OUT OF RACE", style={"background": "#DE6E4B","color":"white"}),
+                                        dbc.CardHeader("OUT OF RACE", style={"background": "#242424","color":"white"}),
                                         dbc.CardBody(
                                                     [
-                                                        html.H6("DESISTÊNCIA", style={"color":"#DE6E4B"}, className="card-title"),
-                                                        html.H4(id="total_offpiste",children=total_gaveup),
+                                                        html.H6("DESISTÊNCIA", style={"color":"#242424"}, className="card-title"),
+                                                        html.H4(id="total_offpiste"),
                                                     ],
 
                                         ),
@@ -180,14 +167,96 @@ app.layout = dbc.Container(
 
                     width=2,xs=12, sm=12,md=12,lg=6,xl=2, 
                 ),
+                dbc.Col(
+                    dbc.Row(dcc.Graph(id='pie')),
+                width=3,xs=12, sm=12,md=12,lg=12,xl=3,
+                ),
 
             ],
         ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dbc.Row(
+                            [
+                                dbc.Col(width=4,xs=12, sm=12,md=4,lg=4,xl=4),
+                                dbc.Col(
+                                    dbc.Row(
+                                        [
+                                            dbc.Row(dbc.Col(width=12),),
+                                            dbc.Row(html.H6("POWERED BY VOST PORTUGAL",style={"align":"center"}),),
+                                            dbc.Row(html.H6("VOST PORTUGAL for ACP MOTORSPORTS",style={"align":"center"}),),
+                                            dbc.Row(html.H6("CC BY-NC-SA 2022",style={"align":"center"}),),
+                                        ],
+                                    ),
+                                ),
+                            ],
+                         style={"height": "30%", "background-color": "#242424"},
+                        ),
+                    ],
+                ),
+                
+            ], 
+        style={"height": "30%", "background-color": "#242424"},
+        ),    
     ],
     style={"width":"100vw","height": "97vh"},
 )
 
-# TEST DATAFRAME
+
+# DEFINE CALL BACKS 
+
+@app.callback(
+    Output(component_id="map",component_property="figure"),                           # returns map
+    Output(component_id="totals",component_property="children"),                    # returns variable
+    Output(component_id="total_warnings",component_property="children"),                     # returns variable
+    Output(component_id="total_breakdowns",component_property="children"),                 # returns variable
+    Output(component_id="total_mechs",component_property="children"),               # returns variable
+    Output(component_id="total_outs",component_property="children"),                             # returns table
+    Output(component_id="total_offpiste",component_property="children"),                             # returns table
+    Output(component_id="pie",component_property="figure"),  
+    Input(component_id="interval-component", component_property="n_intervals"),             # Triggers Call Back based on time update
+
+)
+
+
+# WHAT HAPPENS WHEN CALL BACK IS TRIGGERED
+def confirmUupdate(value):
+
+    # DATA TREATMENT
+
+
+    df_ss1_cc = pd.read_csv('ss1_cc.csv')
+
+
+    df_live_incidents = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vT5ZYA_C5ziVBuAAhRI0LKOMSjkpr2A157Q_WNENhgFVsivnRil4Au3lXE0r_QOg1RnE5HRcN_k29Ej/pub?gid=1319524241&single=true&output=csv')
+    df_live_cc = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vT5ZYA_C5ziVBuAAhRI0LKOMSjkpr2A157Q_WNENhgFVsivnRil4Au3lXE0r_QOg1RnE5HRcN_k29Ej/pub?gid=89946936&single=true&output=csv')
+
+
+
+    totals = str(round(df_live_incidents['total_incidents'].sum()))
+
+    total_warnings = df_live_incidents.loc[df_live_incidents['type'] == "WARNING", 'total_incidents'].sum()
+
+    total_breakdowns = df_live_incidents.loc[df_live_incidents['type'] == "AVARIA", 'total_incidents'].sum()
+
+    total_mech = df_live_incidents.loc[df_live_incidents['type'] == "AVARIA MECÂNICA", 'total_incidents'].sum()
+
+    total_out = df_live_incidents.loc[df_live_incidents['type'] == "DESPISTE", 'total_incidents'].sum()
+
+    total_gaveup = df_live_incidents.loc[df_live_incidents['type'] == "DESISTÊNCIA", 'total_incidents'].sum()
+
+
+    fig = px.scatter_mapbox(df_live_cc, lat="lat", lon="lon", size='reports',hover_name="reporter", hover_data=["reports"],
+                            color_discrete_sequence=["red"], zoom=8)
+    fig.update_layout(mapbox_style="carto-darkmatter")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+    pie_chart = px.pie(df_live_incidents,names='type',values='total_incidents',hole=0.6,color='type',color_discrete_map=color_map)
+
+
+    return fig, totals, total_warnings, total_breakdowns, total_mech, total_out, total_gaveup, pie_chart
 
 
 
@@ -196,7 +265,7 @@ app.layout = dbc.Container(
 # -------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    app.run_server(host='0.0.0.0', port=8081, debug=True)
+    app.run_server(host='0.0.0.0', port=8081, debug=False)
 
 # -------------------------------------------------------------------------------------
 # --------------------------------  THE END       -------------------------------------
